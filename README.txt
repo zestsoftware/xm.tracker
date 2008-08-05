ab34fe2df6aebe07eef8122a8a29884c040cc580
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2008-08-05 12:25:40 +0000

    Initial import of the xm.tracker package
    svn path=/xm.tracker/trunk/; revision=69497

---
+++ README.txt
@@ -0,0 +1,14 @@
+Introduction
+============
+
+This package provides a user interface to track time based on the concepts of
+gtimelog. When starting your working day you start a timer. Each time that you
+change to a different tasks you log an entry of what you were working on.
+
+In eXtreme Management we book our hours on tasks. This tracker allows you to
+select a number of tasks from the list of tasks that are assigned to you.
+The selected tasks are shown in the time tracker.
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+versionfile = open(os.path.join('Products', 'eXtremeManagement', 'version.txt'))
+version = versionfile.read().strip()
+versionfile.close()
+
+setup(name='xm.tracker',
+      version=version,
+      description="This package provides a time tracker based on the concepts of gtimelog",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='xm timetracker',
+      author='Jean-Paul Ladage',
+      author_email='j.ladage@zestsoftware.nl',
+      url='https://svn.plone.org/svn/collective/xm.timetracker',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['xm'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
