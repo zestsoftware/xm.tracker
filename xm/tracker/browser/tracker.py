670bf781e09219ac000f3ebb1d9d14e506f376e4
Maurits van Rees <maurits@vanrees.org>
2008-08-14 15:03:11 +0000

    Allow to remove entries.
    svn path=/xm.tracker/trunk/; revision=69917

--- xm/tracker/browser/tracker.py
+++ xm/tracker/browser/tracker.py
@@ -2,7 +2,6 @@ from random import random
 import math
 
 import mx.DateTime
-import transaction
 from AccessControl import Unauthorized
 from Acquisition import aq_inner, Explicit
 from Products.Five.browser import BrowserView
@@ -182,6 +181,33 @@ class TrackTime(TrackerView):
         tracker.starttime = current_time
         msg = _(u'msg_added_entry', default=u'Added entry')
         IStatusMessage(self.request).addStatusMessage(msg, type="info")
+        self.request.response.redirect('@@tracker')
+
+
+class RemoveEntry(TrackerView):
+    """This view removes an entry for a given task"""
+
+    def __call__(self):
+        uid = self.request.get('uid')
+        entry_number = int(self.request.get('entry_number'))
+        tracker = self.tracker()
+        task = tracker.get_task(uid)
+        if task is None:
+            msg = _(u'msg_no_task_found',
+                    default=u'No task found with this UID')
+            IStatusMessage(self.request).addStatusMessage(msg, type="error")
+            self.request.response.redirect('@@tracker')
+            return
+
+        try:
+            task.entries.pop(entry_number)
+        except IndexError:
+            msg = _(u'msg_remove_entry_failed',
+                    default=u'Failed to remove entry')
+            IStatusMessage(self.request).addStatusMessage(msg, type="error")
+        else:
+            msg = _(u'msg_remove_entry_success', default=u'Removed entry')
+            IStatusMessage(self.request).addStatusMessage(msg, type="info")
         self.request.response.redirect('@@tracker')
 
 
