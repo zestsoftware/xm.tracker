287bb43c09d836bf09d14d467ad73afc70be05d3
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2008-08-18 14:12:07 +0000

    Moved entry related view to a separate module
    svn path=/xm.tracker/trunk/; revision=70118

--- xm/tracker/browser/tracker.py
+++ xm/tracker/browser/tracker.py
@@ -196,33 +196,6 @@ class TrackTime(TrackerView):
         self.request.response.redirect('@@tracker')
 
 
-class RemoveEntry(TrackerView):
-    """This view removes an entry for a given task"""
-
-    def __call__(self):
-        uid = self.request.get('uid')
-        entry_number = int(self.request.get('entry_number'))
-        tracker = self.tracker()
-        task = tracker.get_task(uid)
-        if task is None:
-            msg = _(u'msg_no_task_found',
-                    default=u'No task found with this UID')
-            IStatusMessage(self.request).addStatusMessage(msg, type="error")
-            self.request.response.redirect('@@tracker')
-            return
-
-        try:
-            task.entries.pop(entry_number)
-        except IndexError:
-            msg = _(u'msg_remove_entry_failed',
-                    default=u'Failed to remove entry')
-            IStatusMessage(self.request).addStatusMessage(msg, type="error")
-        else:
-            msg = _(u'msg_remove_entry_success', default=u'Removed entry')
-            IStatusMessage(self.request).addStatusMessage(msg, type="info")
-        self.request.response.redirect('@@tracker')
-
-
 class Book(TrackerView):
     """ This view stores a booking for a given task """
 
