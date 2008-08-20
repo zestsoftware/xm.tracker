ea1146d960a55f58d368a28eb6b588ed5b44b17f
Maurits van Rees <maurits@vanrees.org>
2008-08-20 09:23:47 +0000

    Simplify test setup as we use the XMLayer anyway.
    svn path=/xm.tracker/trunk/; revision=70188

--- xm/tracker/tests/base.py
+++ xm/tracker/tests/base.py
@@ -1,29 +1,8 @@
 from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-import xm.tracker
-from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase.layer import onsetup
 from Products.eXtremeManagement.tests.base import XMLayer
-
-ztc.installProduct('Poi')
-ztc.installProduct('eXtremeManagement')
-
-
-@onsetup
-def xm_setup():
-    """Set up our Plone Site.
-    """
-    fiveconfigure.debug_mode = True
-    import xm.booking
-    zcml.load_config('configure.zcml', xm.booking)
-    import xm.portlets
-    zcml.load_config('configure.zcml', xm.portlets)
-    fiveconfigure.debug_mode = False
-
-xm_setup()
-ptc.setupPloneSite(products=['Products.eXtremeManagement'])
 
 
 class TestCase(ptc.PloneTestCase):
