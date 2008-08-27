443ab16aa8b86f81213f6597c07f40e25b08a6a6
Reinout van Rees <reinout@vanrees.org>
2008-08-27 08:27:01 +0000

    Typos in comments
    svn path=/xm.tracker/trunk/; revision=70715

--- xm/tracker/rebuild_i18n.sh
+++ xm/tracker/rebuild_i18n.sh
@@ -3,14 +3,11 @@ PRODUCTNAME=tracker
 I18NDOMAIN=tracker
 
 # Synchronise the .pot with the templates.
-# Also merge it with generated.pot, which includes the items
-# from schema.py
 i18ndude rebuild-pot --pot locales/${PRODUCTNAME}.pot --create ${I18NDOMAIN} .
 
 # Synchronise the resulting .pot with the .po files
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
 
-# Zope3 is lazy so we have to comile the po files ourselves
+# Zope3 is lazy so we have to compile the po files ourselves
 # When dropping Plone 3.0 support we can remove mo files from svn, because Zope got smart.
-
 msgfmt -o locales/nl/LC_MESSAGES/${PRODUCTNAME}.mo locales/nl/LC_MESSAGES/${PRODUCTNAME}.po
