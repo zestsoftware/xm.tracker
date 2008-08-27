0e265cba84acc50449cc7e66fe201342151448b0
Reinout van Rees <reinout@vanrees.org>
2008-08-27 09:52:45 +0000

    Added kss-only version of the task selection, to be used for unassigned entries
    svn path=/xm.tracker/trunk/; revision=70740

--- xm/tracker/browser/ksstracker.py
+++ xm/tracker/browser/ksstracker.py
@@ -1,11 +1,16 @@
-import mx.DateTime
 from Acquisition import aq_inner
 from kss.core import kssaction
 from plone.app.kss.plonekssview import PloneKSSView
+from zope.annotation.interfaces import IAnnotations
+from zope.component import getMultiAdapter
+import mx.DateTime
 
 from xm.tracker import XMTrackerMessageFactory as _
+from xm.tracker.tracker import Tracker
 from xm.tracker.browser.tracker import add_entry
+from xm.tracker.browser.tracker import TRACKER_KEY
 from xm.tracker.browser.viewlets import TaskViewlet
+
 
 def get_tracker(context):
     tracker_view = context.restrictedTraverse('@@tracker')
@@ -44,7 +49,7 @@ class KSSStop(PloneKSSView):
 
 class KSSTrackTime(PloneKSSView):
     """kss view for adding an entry to a task"""
-    
+
     @kssaction
     def track_time(self, uid, text):
         context = aq_inner(self.context)
@@ -68,11 +73,46 @@ class KSSTrackTime(PloneKSSView):
 
 class KSSSelectTasks(PloneKSSView):
     """KSS view for selecting tasks"""
-    
+
     @kssaction
     def select_tasks(self):
         context = aq_inner(self.context)
         view = context.restrictedTraverse('@@tracker_select_tasks')
         html = view()
+        core = self.getCommandSet("core")
+        core.insertHTMLBefore('#content', html)
+
+
+class KSSSelectTasksForUnassigned(PloneKSSView):
+    """KSS view for selecting tasks for unassigned entries"""
+
+    def tracker(self):
+        # Copied from tracker.TrackerView pending later refactoring.
+        context = aq_inner(self.context)
+        portal_state = getMultiAdapter(
+            (context, self.request), name=u'plone_portal_state')
+        if portal_state.anonymous():
+            return None
+        member = portal_state.member()
+        annotations = IAnnotations(member)
+        tracker = annotations.get(TRACKER_KEY, None)
+        if tracker is None or not hasattr(tracker, 'unassigned'):
+            tracker = Tracker()
+            annotations[TRACKER_KEY] = tracker
+
+        return tracker
+
+    def todo_tasks_per_project(self):
+        """For now we just get all to-do tasks here.
+        """
+        # Copied from tracker.AddTasks
+        context = aq_inner(self.context)
+        mytask_details = getMultiAdapter(
+            (context, self.request), name=u'mytask_details')
+        return mytask_details.projects()
+
+    @kssaction
+    def __call__(self):
+        html = self.index() # Uses templates/select.pt
         core = self.getCommandSet("core")
         core.insertHTMLBefore('#content', html)
