9a2af2da9f25db0be000e281490f7c028a104f49
Maurits van Rees <maurits@vanrees.org>
2008-09-16 15:33:54 +0000

    Fix up the page as it appears on the cheese shop: including README.txt, xm/tracker/HISTORY.txt and docs/INSTALL.txt (mostly for mx.DateTime).
    svn path=/xm.tracker/trunk/; revision=71968

--- README.txt
+++ README.txt
@@ -8,7 +8,3 @@ change to a different tasks you log an entry of what you were working on.
 In eXtreme Management we book our hours on tasks. This tracker allows you to
 select a number of tasks from the list of tasks that are assigned to you.
 The selected tasks are shown in the time tracker.
-
-
-
-
--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 from setuptools import setup, find_packages
 import os
 
-versionfile = open(os.path.join('xm', 'tracker', 'version.txt'))
-version = versionfile.read().strip()
-versionfile.close()
+
+def read_package_file(filename):
+    file = open(os.path.join('xm', 'tracker', filename))
+    text = file.read().strip()
+    file.close()
+    return text
+
+
+version = read_package_file('version.txt')
+history = read_package_file('HISTORY.txt')
+
+readmefile = open('README.txt')
+readme = readmefile.read().strip()
+readmefile.close()
+
+installfile = open(os.path.join('docs', 'INSTALL.txt'))
+install = installfile.read().strip()
+installfile.close()
+
+long_description = readme + '\n\n' + history + '\n\n' + install
 
 setup(name='xm.tracker',
       version=version,
-      description="This package provides a time tracker based on the concepts of gtimelog",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description="A time tracker based on the concepts of gtimelog",
+      long_description=long_description,
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
