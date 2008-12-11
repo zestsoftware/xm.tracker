652690c4162d3b895d274fd20cb65b6451d20744
Maurits van Rees <maurits@vanrees.org>
2008-12-11 13:12:40 +0000

    Moved "eXtremeManagement: View Tracker" permission to Products.eXtremeManagement as we do not actually use that permission in the xm.tracker package.
    svn path=/xm.tracker/trunk/; revision=77235

--- xm/tracker/__init__.py
+++ xm/tracker/__init__.py
@@ -1,11 +1,6 @@
 from zope.i18nmessageid import MessageFactory
-from Products.CMFCore.permissions import setDefaultRoles
-
 
 XMTrackerMessageFactory = MessageFactory('tracker')
-
-setDefaultRoles("eXtremeManagement: View Tracker",
-                ('Projectmanager', 'Employee', 'Manager'))
 
 
 def initialize(context):
