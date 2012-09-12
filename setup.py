73738969e7ae4be3b76432f0920330218de6b3cd
Maurits van Rees <maurits@vanrees.org>
2012-09-12 18:56:05 +0200

    Update setup.py, move files around

--- setup.py
+++ setup.py
@@ -1,40 +1,25 @@
 from setuptools import setup, find_packages
-import os
+import os.path
 
-
-def read_package_file(filename):
-    file = open(os.path.join('xm', 'tracker', filename))
-    text = file.read().strip()
-    file.close()
-    return text
-
-
-version = read_package_file('version.txt')
-history = read_package_file('HISTORY.txt')
-
-readmefile = open('README.txt')
-readme = readmefile.read().strip()
-readmefile.close()
-
-installfile = open(os.path.join('docs', 'INSTALL.txt'))
-install = installfile.read().strip()
-installfile.close()
+history = open('CHANGES.rst').read().strip()
+readme = open('README.txt').read().strip()
+install = open(os.path.join('docs', 'INSTALL.txt')).read().strip()
 
 long_description = readme + '\n\n' + history + '\n\n' + install
 
 setup(name='xm.tracker',
-      version=version,
+      version='1.0.6.dev0',
       description="A time tracker based on the concepts of gtimelog",
       long_description=long_description,
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='xm timetracker',
       author='Jean-Paul Ladage',
       author_email='j.ladage@zestsoftware.nl',
-      url='https://svn.plone.org/svn/collective/xm.timetracker',
+      url='https://plone.org/products/extreme-management-tool/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['xm'],
